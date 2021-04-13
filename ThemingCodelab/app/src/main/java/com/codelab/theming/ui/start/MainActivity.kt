573250b29132759d354d5fd8c76b1d66f1183129
magicKen <i@magicken.com>
2021-04-13 16:32:59 +0800

    working with color

--- ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/MainActivity.kt
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/MainActivity.kt
@@ -22,12 +22,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            Surface {
-                Home()
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      Surface {
+        Home()
+      }
     }
+  }
 }
